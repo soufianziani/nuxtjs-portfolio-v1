80150f74bb56d928632452aea36708777ed4d03c
Soufiane Ziani <61345039+soufianziani@users.noreply.github.com>
2024-07-17 14:23:26 +0100

    FIX : screen width

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="w-full flex flex-col items-center justify-center">
+  <div class="max-w-screen-lg flex flex-col items-center justify-center">
     <Header class="my-div" />
   </div>
-  <Marquee class=" transform my-20 "/>
+  <Marquee class=" w-full transform my-20 "/>
 
-  <div class="min-h-screen  w-full">
+  <div class="min-h-screen  max-w-screen-lg">
     <!-- <div class="flex items-center justify-center my-20">
       <img src="assets/icons/StarBlack.svg" class="w-10" alt="" />
     </div> -->
     <div class="flex justify-center items-center"></div>
     <Experience />
-    <div class="w-full ">
+    <div class="max-w-screen-lg ">
         <!-- <div class="flex items-center justify-center my-10">
       <img src="assets/icons/Logo.svg" class="w-10" alt="" />
     </div> -->
